ce3a3bcdca84c0596adf5b96b08ef33b9e7195da
Deepanshu Kaushik <kaushikdeepanshu034@gmail.com>
2026-04-14 23:57:43 +0530

    Step 1-2: Add Flutter web build and Vercel config
    - Built Flutter web app in release mode
    - Committed build/web directory for deployment
    - Added vercel.json with build/web as outputDirectory
    - Configured SPA routing for index.html

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "333907015"
+    serviceWorkerVersion: "3749325348"
   }
 });
